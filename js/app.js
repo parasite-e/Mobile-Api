64cc49c19191aa768e0c99e39db83f9a73e5dc7b
parasite-e <fardinemonbd@gmail.com>
2022-03-01 21:51:34 +0600

    api error handled

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const toggleDisplayResult = display => {
+// toogling the details display 
+const toggleDetailsDisplay = display => {
     const displayDetails = document.getElementById('display-details');
     displayDetails.style.display = display;
 }
@@ -15,38 +16,44 @@ const searchMobile = () => {
 
     } else {
         inputError.style.display = 'none';
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchName}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayResult(data.data));
     }
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchName}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayResult(data.data));
 
-    toggleDisplayResult('none');
+    toggleDetailsDisplay('none');
 }
 
 // displaying the result
 const displayResult = phones => {
     const displayResult = document.getElementById('display-result');
     displayResult.textContent = '';
-    phones.forEach(phone => {
-        // console.log(phone);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card">        
-                <img src="${phone.image}" class="card-img-top" alt="...">  
-                    <div class="d-flex flex-column align-items-center">
-                      <div class="card-body text-center">
-                       <h5 class="card-title">${phone.phone_name}</h5>
-                       <h6>${phone.brand}</h6>
-                      </div>
-                      <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary w-50 mb-2">Details</button>
+
+    const apiError = document.getElementById('api-error');
+    if (phones.length == 0) {
+        apiError.style.display = 'block';
+    } else {
+        apiError.style.display = 'none';
+        phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card">        
+                    <img src="${phone.image}" class="card-img-top" alt="...">  
+                        <div class="d-flex flex-column align-items-center">
+                          <div class="card-body text-center">
+                           <h5 class="card-title">${phone.phone_name}</h5>
+                           <h6>${phone.brand}</h6>
+                          </div>
+                          <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary w-50 mb-2">Details</button>
+                        </div>
                     </div>
-                </div>
-        
-        `;
-        displayResult.appendChild(div);
-    })
+            `;
+            displayResult.appendChild(div);
+        })
+    }
+
 }
 
 // loading the phone deatails 
@@ -58,7 +65,7 @@ const loadPhoneDetails = phoneId => {
 }
 
 const displayPhoneDetails = phone => {
-    toggleDisplayResult('block');
+    toggleDetailsDisplay('block');
     console.log(phone);
     const displayDetails = document.getElementById('display-details');
     displayDetails.textContent = '';
