e300aa34b8204e531ee2f7cac86eeeb2ef379855
parasite-e <fardinemonbd@gmail.com>
2022-03-01 08:33:22 +0600

    fetchted api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('hi');
+// getting the search value through api
+const searchMobile = () => {
+    const searchField = document.getElementById('search-field');
+    const searchName = searchField.value;
+
+    searchField.value = '';
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchName}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data));
+}
